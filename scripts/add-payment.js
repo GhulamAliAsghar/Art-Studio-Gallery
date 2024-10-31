f7d69eee478abdf55408be1a6bfa9cc7b63fcd60
GhulamAliAsghar <galiasghar9.xwave@gmail.com>
2024-10-31 22:00:43 +0500

    Finalized bookings and payments pages

---
+++ scripts/add-payment.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("saveBtn")
+
+btn.addEventListener("click" , function () {
+    alert("Payment Method Save!!!")
+    window.location.href = "/pages/payment.html"
+})
