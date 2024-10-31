d015f87ef2200d98105324631267bd6b5953aecc
GhulamAliAsghar <galiasghar9.xwave@gmail.com>
2024-10-31 10:38:27 +0500

    Add payment page

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,29 +1,35 @@
-const firstCard = document.getElementById("card-1")
-const secondCard = document.getElementById("card-2")
-const thirdCard = document.getElementById("card-3")
 const firstImg = document.getElementById("img-1")
 const secondImg = document.getElementById("img-2")
 const thirdImg = document.getElementById("img-3")
 
-firstCard.addEventListener("click", function () {
-    firstImg.style.display = "block"
-    secondImg.style.display = "none"
-    thirdImg.style.display = "none"
-    firstCard.style.borderColor = "black"
-})
+document.addEventListener("DOMContentLoaded", function () {
 
-secondCard.addEventListener("click", function () {
-    secondImg.style.display = "block"
-    firstImg.style.display = "none"
-    thirdImg.style.display = "none"
-    secondCard.style.borderColor = "yellow"
-})
+    const firstCard = document.getElementById("card-1")
+    const secondCard = document.getElementById("card-2")
+    const thirdCard = document.getElementById("card-3")
 
-thirdCard.addEventListener("click", function () {
-    thirdImg.style.display = "block"
-    firstImg.style.display = "none"
-    secondImg.style.display = "none"
-    secondCard.style.borderColor = "yellow"
+    firstCard.addEventListener("click", function () {
+        event.stopPropagation();
+        firstImg.style.display = "block"
+        secondImg.style.display = "none"
+        thirdImg.style.display = "none"
+        firstCard.style.borderColor = " !important"
+    })
+
+    secondCard.addEventListener("click", function () {
+        event.stopPropagation();
+        secondImg.style.display = "block"
+        firstImg.style.display = "none"
+        thirdImg.style.display = "none"
+        secondCard.style.borderColor = "yellow"
+    })
+
+    thirdCard.addEventListener("click", function () {
+        thirdImg.style.display = "block"
+        firstImg.style.display = "none"
+        secondImg.style.display = "none"
+        thirdCard.style.borderColor = "yellow"
+    })
 })
 
 
